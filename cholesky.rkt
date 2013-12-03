b68ccb624bd35b392475497b7cf3ae7c6dafde46
Daniel King <daniel.zidan.king@gmail.com>
2013-12-02 21:13:42 -0500

    Fix Cholesky Types
    This involves adding a check to ensure that we never take the square root of a
    negative number. This amounts to a last-minute positive definite check. I'd
    rather do this than actually check for positive definiteness because I don't
    know if Typed Racket will realize that positive definite implies the entries
    will never be imaginary numbers

--- cholesky.rkt
+++ cholesky.rkt
@@ -4,23 +4,38 @@
 
 (define mref matrix-ref)
 
-(: compute-diagonal-entry : ([Matrix Number]
-                             [Matrix Number]
+(: compute-diagonal-entry : ([Matrix Real]
+                             [Matrix Real]
                              Natural
                              ->
-                             Number))
+                             Real))
 (define (compute-diagonal-entry A L k)
-  (sqrt (- (mref A k k)
-           (for/sum: : Number
-                     ([j (in-range 0 k)])
-             (sqr (mref L k j))))))
+  (define entry-squared
+    (- (mref A k k)
+       (for/sum: : Real
+                 ([j (in-range 0 k)])
+         (sqr (mref L k j)))))
+  (if (< entry-squared 0)
+      (error 'cholesky
+             (string-append
+              "This matrix isn't positive definite! "
+              "Computing diagonal ~a, A_kk is ~a. "
+              "k'th partial row is ~a. "
+              "Entry squared values is ~a. ")
+             k
+             (mref A k k)
+             (for/vector: : [Vectorof Real]
+                          ([j (in-range 0 k)])
+               (mref L k j))
+             entry-squared)
+      (sqrt entry-squared)))
 
-(: build-cholesky-column : ([Matrix Number]
-                            [Matrix Number]
+(: build-cholesky-column : ([Matrix Real]
+                            [Matrix Real]
                             Natural
                             Natural
                             ->
-                            [Matrix Number]))
+                            [Matrix Real]))
 ;; Given a positive definite matrix A and a partial cholesky factor of A such
 ;; that all columns less than k are already properly filled out
 (define (build-cholesky-column A L k n)
@@ -29,26 +44,26 @@
 
   (for/array: #:shape (vector n 1)
               ([i (in-range 0 n)])
-    : Number
+    : Real
     (cond [(< i k) 0]
           [(= i k) diagonal-entry]
           [else
            (* (/ 1 diagonal-entry)
               (- (mref A i k)
-                 (for/sum: : Number
+                 (for/sum: : Real
                            ([j (in-range 0 k)])
                    (* (mref L i j) (mref L k j)))))])))
 
-(: cholesky : ((Matrix Number) -> (Matrix Number)))
+(: cholesky : ((Matrix Real) -> (Matrix Real)))
 (define (cholesky A)
   (define mref matrix-ref)
   (define n (matrix-num-rows A))
 
-  (for*/fold: : [Matrix Number]
-              ([L : [Array Number] (build-cholesky-column A
-                                                          (make-matrix 0 0 0)
-                                                          0
-                                                          n)])
+  (for*/fold: : [Matrix Real]
+              ([L : [Matrix Real] (build-cholesky-column A
+                                                         (make-matrix 0 0 0)
+                                                         0
+                                                         n)])
               ([k : Natural (in-range 1 n)])
     (matrix-augment (list L
                           (build-cholesky-column A L k n)))))
