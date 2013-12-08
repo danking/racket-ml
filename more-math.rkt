cdb7a993767887b37e91f81ffb417e7b2e5c3306
Daniel King <daniel.zidan.king@gmail.com>
2013-12-07 20:31:59 -0500

    Add log behavior to `multivariate-beta` via the #:log keyword

--- more-math.rkt
+++ more-math.rkt
@@ -5,7 +5,10 @@
 (provide multivariate-beta
          )
 
-(: multivariate-beta : [Vectorof Real] -> Real)
-(define (multivariate-beta vec)
-  (/ (for/product: : Real ([v vec]) (gamma v))
-     (gamma (for/sum: : Real ([v vec]) v))))
+(: multivariate-beta : ([Vectorof Real] [#:log? Boolean] -> Real))
+(define (multivariate-beta vec #:log? [log? #f])
+  (if log?
+      (- (for/sum: : Real ([v vec]) (log-gamma v))
+         (log-gamma (for/sum: : Real ([v vec]) v)))
+      (/ (for/product: : Real ([v vec]) (gamma v))
+         (gamma (for/sum: : Real ([v vec]) v)))))
